26f01fae3338b2ef04617f88f853254a083df153
José de Assis <josedeassisfilho@gmail.com>
2018-03-21 09:59:23 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/joseassis/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author José de Assis
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
