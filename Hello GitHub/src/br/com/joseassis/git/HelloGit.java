68fd074a9d0407ce6a23499f1a49aed30fbc4816
Professor José de Assis <josedeassisfilho@gmail.com>
2022-02-28 12:48:30 -0300

    Update HelloGit.java

--- Hello GitHub/src/br/com/joseassis/git/HelloGit.java
+++ Hello GitHub/src/br/com/joseassis/git/HelloGit.java
@@ -4,14 +4,14 @@
 package br.com.joseassis.git;
 
 /**
- * @author José de Assis
- * Exemplo de integração do Eclipse com o GitHub
+ * @author Professor JosÃ© de Assis
+ * Exemplo de integraÃ§Ã£o do Eclipse com o GitHub
  */
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Professor José de Assis");
+		System.out.println("Professor JosÃ© de Assis");
 	}
 
 }
